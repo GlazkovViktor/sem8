d5b6e44c9871dec78faa54f9b1b3ecbb2b33a2d3
hockeymiass21@gmail.com <hockeymiass21@gmail.com>
2023-01-28 20:55:49 +0500

    jhjhf

---
+++ controller.py
@@ -0,0 +1,20 @@
+import view
+import model
+
+
+def start():
+    model.set_class(view.input_class())
+    model.set_subject(view.input_subject())
+    model.open_file()
+    while True:
+        journal = model.get_journal()
+        view.list_of_child(journal)
+        student = view.who_answer()
+        if student == 'exit':
+            break
+        mark = view.what_mark()
+        model.student_mark(student, mark)
+    model.save_file()
+      
+
+
---
+++ view.py
@@ -0,0 +1,23 @@
+import model
+
+
+def input_class():
+    return input('С каким классом работем? ').upper()
+
+
+def input_subject():
+    return input('Какой предмет? ').lower()
+
+    
+
+def who_answer():
+    return input('Кто будет отвечать? ')
+
+
+def what_mark():
+    return int(input('На какую оценку ответил? '))
+        
+
+def list_of_child(journal: dict):
+    for i, child in enumerate(journal, 1):
+        print(f'{i}. {child:20} {journal.get(child)}')
